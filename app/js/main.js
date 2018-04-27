fb9533095cbad856a094e2e93e41d16f6efa75db
rockn1989 <gold_100@bk.ru>
2018-04-27 19:03:19 +0300

    start

---
+++ app/js/main.js
@@ -0,0 +1,85 @@
+'use strict';
+
+$(function() {
+
+
+	// Показывать форму на мобильных устройствах
+
+	var $mobileForm = $('.mobile-form');
+
+	$('.js__show-form').on('click', function (e) {
+		e.preventDefault();
+		$mobileForm.slideToggle('350').find('input').focus();
+	})
+
+
+	// Маска формы
+
+	$('.js__input-phone').mask('+7 999 999-99-99', {clearIfNotMatch: true}).focus(function (e) {
+		if (!$(this).val()) {
+			$(this).val('+7 ');
+		}
+	});
+
+
+	// Валидация формы
+
+	if($('form').is('.default-form')) {
+
+		$('.default-form').validate({
+			rules: {
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				email: "Обязательноe поле",
+			},
+		});
+	};
+
+
+	// Открытие мобильного подменю
+
+	$('.js__menu-sublist-toggle').on('click', function (e) {
+		e.preventDefault();
+		var self = $(this),
+			blockParent = self.parent('a'),
+			siblingsList = blockParent.siblings('.menu-sublist');
+
+		if(blockParent.hasClass('open')) {
+			siblingsList.stop().slideUp('350', function () {
+				blockParent.removeClass('open');
+			});
+		} else {
+			siblingsList.stop().slideDown('350', function () {
+				blockParent.addClass('open');
+			});
+		}
+
+		self.toggleClass('open');
+	});
+
+
+	// Открытие мобильного подменю в футере
+
+	$('[data-role="toggle-list"] i').on('click', function (e) {
+		e.preventDefault();
+		var self = $(this),
+			blockParent = self.parent('[data-role="toggle-list"]'),
+			siblingsList = blockParent.parent().find('.footer__list');
+
+		self.toggleClass('open');
+		siblingsList.stop().slideToggle('350');
+	});
+
+	// Отключение UIKIT анимации для мобильных устройств
+
+	UIkit.on('beforeready.uk.dom', function () {
+		if (UIkit.$win.width() < 767 && $('html').hasClass('uk-touch')) {
+			UIkit.$('[data-uk-scrollspy]').removeAttr('data-uk-scrollspy');
+		};
+	});
+
+});
