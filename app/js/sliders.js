ef751dbc642d6252c2be10978e08b59c8216daef
rockn1989 <gold_100@bk.ru>
2018-05-17 18:41:25 +0300

    адаптация главной

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -12,7 +12,38 @@ $(function () {
 		speed: 1000,
 		arrows: true,
 		prevArrow: '<div class="slide-prev"><i class="icon-arrow-left-small"></i></div>',
-		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>'
+		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>',
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 960,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true,
+					adaptiveHeight: true
+				}
+			}
+		]
 	});
 
 
@@ -26,7 +57,38 @@ $(function () {
 		autoplaySpeed: 3000,
 		speed: 1000,
 		prevArrow: '<div class="slide-prev"><i class="icon-arrow-left-small"></i></div>',
-		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>'
+		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>',
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true,
+					adaptiveHeight: true
+				}
+			}
+		]
 	});
 
 	// Слайдер нас рекомендуют
@@ -41,7 +103,38 @@ $(function () {
 		//centerMode: true,
 		//centerPadding: '90px',
 		prevArrow: '<div class="slide-prev"><i class="icon-arrow-left-small"></i></div>',
-		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>'
+		nextArrow: '<div class="slide-next"><i class="icon-arrow-right-small"></i></div>',
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true,
+					adaptiveHeight: true
+				}
+			}
+		]
 	})
 
 });
